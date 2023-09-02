3a108b048610683af40c86899dc6c0d1d549f1a0
Muhammad Khattab <msskzx@gmail.com>
2023-09-02 21:56:43 +0200

    change feature

--- src/my_train.sh
+++
@@ -1,4 +0,0 @@
-DATASET=Synthetic_NeRF_Lego
-
-# Distilling tacher into KiloNeRF model
-python local_distill.py cfgs/paper/distill/$DATASET.yaml
--- src/render_to_screen.sh
+++
@@ -1,3 +0,0 @@
-# Real-time rendering of a trained KiloNeRF model to the screen
-DATASET=Synthetic_NeRF_Lego
-python run_nerf.py cfgs/paper/finetune/$DATASET.yaml -rcfg cfgs/render/render_to_screen.yaml
